ff63402a7dba21f4605bda5c368796494639f1b3
Spiros <spiroskleft@gmail.com>
2017-05-15 12:11:16 +0300

    Subject-Subject Join

--- src/main/java/rdf/joins/RdfJoins.java
+++ src/main/java/rdf/joins/RdfJoins.java
@@ -16,19 +16,24 @@ public class RdfJoins {
      * when triples in verticalPartitioning (VP)
      * ?s p1 o1
      *
-     * @param object
-     * @param predicate
+     * @param object1
+     * @param object2
+     * @param predicate1
+     * @param predicate2
      */
-    public static void findSubjectSubjectJoin(String object, String predicate, SparkSession sparkSession) {
+    public static void findSubjectSubjectJoin(String predicate1, String predicate2, String object1, String object2, SparkSession sparkSession) {
         //The predicate will tell us the file that we must take
         //Φορτώνουμε το αρχειο σε ένα Dataset
-        Dataset<Row> df = sparkSession.read().csv(ReadPropertiesFile.inputPath + predicate + ".csv");
+        Dataset<Row> df = sparkSession.read().csv(ReadPropertiesFile.inputPath + predicate1 + ".csv");
+        Dataset<Row> df2 = sparkSession.read().csv(ReadPropertiesFile.inputPath + predicate2 + ".csv");
 
-        df.createOrReplaceTempView("tableName");
+        df.createOrReplaceTempView("tableName1");
+        df2.createOrReplaceTempView("tableName2");
         //Κάνουμε προβολή των δεδομένων
-        sparkSession.sql("SELECT _c0 as subject " +
-                "FROM tableName " +
-                "where _c1='" + object + "'").show();
+        sparkSession.sql("SELECT _c0 as subject1, _c1 as subject2" +
+                "FROM tableName1, tableName2 " +
+                "where tableName1._c0='" + object1 + " AND tableName2._c0=" + object2 +
+                " AND tableName1.subject1 = tableName2.subject2" +"'").show();
     }
 
 
